10b799000bf77bcb2e7d5a7a294017f21542696b
Nguyễn Hữu Phúc <huuphuc95@outlook.com>
2020-05-27 00:44:44 +0700

    clean up point board

--- components/PointBoard.tsx
+++ components/PointBoard.tsx
@@ -1,25 +1,9 @@
 import React from "react";
-
 import { EuiCard, EuiIcon, EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
-
-const icons = ["Beats", "Cloud", "Logging", "Kibana"];
-
-const cardNodes = icons.map(function (item, index) {
-  return (
-    <EuiFlexItem key={index}>
-      <EuiCard
-        icon={<EuiIcon size="xxl" type={`logo${item}`} />}
-        title={`Elastic ${item}`}
-        description="Example of a card's description. Stick to one or two sentences."
-        onClick={() => window.alert("Card clicked")}
-      />
-    </EuiFlexItem>
-  );
-});
 
 const PointBoard = () => {
   return (
-    <EuiFlexGroup gutterSize={"s"} style={{ width: 900, padding: 20 }}>
+    <EuiFlexGroup gutterSize={"xl"} style={{ width: 900, padding: 20 }}>
       <EuiFlexItem>
         <EuiCard
           title="Incoming"
