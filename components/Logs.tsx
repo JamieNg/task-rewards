9eb621daf48c93ea90abfbe433381e346ea0fec7
Nguyen Huu Phuc <phuc-huu.nguyen@idealogic.com.vn>
2020-05-27 15:00:03 +0700

    init simple log table

--- components/Logs.tsx
+++ components/Logs.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import {EuiBasicTable} from "@elastic/eui";
+import { EuiBasicTable } from "@elastic/eui";
+import { EuiBasicTableColumn } from "@elastic/eui/src/components/basic_table/basic_table";
+import { EuiTableFieldDataColumnType } from "@elastic/eui/src/components/basic_table/table_types";
+
+const data = [{ name: "Hi" }];
 
 const Logs = () => {
-  return <EuiBasicTable columns={} items={}>
-    
-  </EuiBasicTable>;
+  const columns: EuiTableFieldDataColumnType<any>[] = [
+    { field: "name", name: "Name" },
+  ];
+
+  return <EuiBasicTable columns={columns} items={data}></EuiBasicTable>;
 };
 
 export default Logs;
